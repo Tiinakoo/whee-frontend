70345ddabd06e12813e06b65a4d0b7b6be0f4e14
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-22 19:06:59 +0200

    Start showing slogan in Navigation

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,10 +1,17 @@
 import React from "react";
 import logo from "../images/logo.png";
 import styles from "./Navigation.module.scss";
+import translations from "../translations";
+import getLanguage from "../doings/getLanguage/getLanguage";
+import BodyText from "../components/shared/BodyText/BodyText";
+
+const localTranslation = translations[getLanguage()];
 
 const Navigation = () => (
   <div className={styles.navigation}>
     <img src={logo} alt="Logo" />
+
+    <BodyText size="md">{localTranslation.slogan}</BodyText>
   </div>
 );
 
---
+++ src/translations.js
@@ -0,0 +1,5 @@
+export default {
+  en: {
+    slogan: "The most definitive shape store in the world",
+  },
+};
