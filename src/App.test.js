676a4a3ef05595dac0a88944cbf7a5bafdbc5e9e
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-22 12:18:26 +0200

    Remove unnecessary logos

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import { shallow } from "enzyme";
+import React from "react";
+import "../setupUnitTests";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<App />);
+  });
+
+  it("renders", () => {
+    expect(component).toMatchSnapshot();
+  });
 });
