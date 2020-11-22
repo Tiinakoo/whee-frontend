c626762b08cd73d7828c5074f7c84a7c3f520ab3
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-22 20:13:44 +0200

    Make BodyText able to work with different fonts

--- src/components/shared/BodyText/BodyText.js
+++ src/components/shared/BodyText/BodyText.js
@@ -3,10 +3,21 @@ import upperFirst from "lodash/fp/upperFirst";
 import styles from "./BodyText.module.scss";
 import classNames from "classnames";
 
-const BodyText = ({ size = "sm", className, children, ...props }) => {
+const BodyText = ({
+  size = "sm",
+  font = "body",
+  centeredVertically,
+  className,
+  children,
+  ...props
+}) => {
   const classes = classNames([
     styles.bodyText,
+    styles[`bodyText__font${upperFirst(font)}`],
     styles[`bodyText__size${upperFirst(size)}`],
+    {
+      [styles.bodyText__centeredVertically]: centeredVertically,
+    },
     className,
   ]);
 
--- src/translations.js
+++ src/translations.js
@@ -1,5 +1,6 @@
 export default {
   en: {
+    logo: "Logo",
     slogan: "The most definitive shape store in the world",
   },
 };
