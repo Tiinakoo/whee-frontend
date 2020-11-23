8e588c40ac744c40db7cffa3e11cddcb27010535
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-23 18:48:22 +0200

    Make Flex support space between styling

--- src/components/shared/Flex/Flex.js
+++ src/components/shared/Flex/Flex.js
@@ -8,24 +8,26 @@ const Flex = ({
   centered,
   centeredVertically = centered,
   centeredHorizontally = centered,
+  spaceBetween,
   lineUpHorizontally,
   lineUpHorizontallyTo,
   ...props
 }) => {
-  const classNameParameters = [
+  const classes = classNames([
     styles.flex,
     {
       [styles.flex__centeredVertically]: centeredVertically,
       [styles.flex__centeredHorizontally]: centeredHorizontally,
+      [styles.flex__spaceBetween]: spaceBetween,
       [styles.flex__lineUpHorizontallyToMdScreen]:
         lineUpHorizontallyTo === "md",
       [styles.flex__lineUpHorizontally]: lineUpHorizontally,
     },
     className,
-  ];
+  ]);
 
   return (
-    <div className={classNames(classNameParameters)} {...props}>
+    <div className={classes} {...props}>
       {children}
     </div>
   );
