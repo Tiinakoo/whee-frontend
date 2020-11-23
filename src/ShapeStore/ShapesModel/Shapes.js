c77d036456fd4a08f45bc6a81fd4884dd518bc75
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-23 18:51:20 +0200

    Start showing more content in UI for shapes

--- src/ShapeStore/ShapesModel/Shapes.js
+++ src/ShapeStore/ShapesModel/Shapes.js
@@ -1,9 +1,27 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
+import Flex from "../../components/shared/Flex/Flex";
+import Icon from "../../components/shared/Icon/Icon";
+import Heading from "../../components/shared/Heading/Heading";
+import BodyText from "../../components/shared/BodyText/BodyText";
+import Element from "../../components/shared/Element/Element";
 
 const Shapes = ({ shapesModel: { shapes } }) => {
-  return shapes.map(({ id, name }) => <div key={id}>{name}</div>);
+  return shapes.map(({ id, name, description }) => (
+    <div key={id}>
+      <Flex lineUpHorizontally centeredVertically>
+        <Icon iconName={name} />
+
+        <Element minWidth="md">
+          <Flex>
+            <Heading>{name}</Heading>
+
+            <BodyText>{description}</BodyText>
+          </Flex>
+        </Element>
+      </Flex>
+    </div>
+  ));
 };
 
 export default observer(Shapes);
-
