5cd1b188711747999fb988af0208d9251021d1de
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-23 19:54:41 +0200

    Show price and button for adding to cart in UI

--- src/ShapeStore/ShapesModel/Shapes.js
+++ src/ShapeStore/ShapesModel/Shapes.js
@@ -5,22 +5,41 @@ import Icon from "../../components/shared/Icon/Icon";
 import Heading from "../../components/shared/Heading/Heading";
 import BodyText from "../../components/shared/BodyText/BodyText";
 import Element from "../../components/shared/Element/Element";
+import PrimaryButton from "../../components/shared/PrimaryButton/PrimaryButton";
+import translations from "../../translations";
+import getLanguage from "../../doings/getLanguage/getLanguage";
+
+const localTranslation = translations[getLanguage()];
 
 const Shapes = ({ shapesModel: { shapes } }) => {
-  return shapes.map(({ id, name, description }) => (
-    <div key={id}>
-      <Flex lineUpHorizontally centeredVertically>
-        <Icon iconName={name} />
+  return shapes.map(({ id, name, description, price, currency }) => (
+    <Element padding="sm" key={id}>
+      <Flex spaceBetween lineUpHorizontallyTo="md">
+        <Element minWidth="sm">
+          <Flex lineUpHorizontally centeredVertically>
+            <Icon iconName={name} />
 
-        <Element minWidth="md">
+            <Element minWidth="md">
+              <Flex>
+                <Heading>{name}</Heading>
+
+                <BodyText>{description}</BodyText>
+              </Flex>
+            </Element>
+          </Flex>
+        </Element>
+
+        <Element minWidth="sm">
           <Flex>
-            <Heading>{name}</Heading>
+            <Heading alignRight>
+              {price} {currency}
+            </Heading>
 
-            <BodyText>{description}</BodyText>
+            <PrimaryButton>{localTranslation.addToCart}</PrimaryButton>
           </Flex>
         </Element>
       </Flex>
-    </div>
+    </Element>
   ));
 };
 
--- src/translations.js
+++ src/translations.js
@@ -2,5 +2,6 @@ export default {
   en: {
     logo: "Logo",
     slogan: "The most definitive shape store in the world",
+    addToCart: "Add to cart",
   },
 };
