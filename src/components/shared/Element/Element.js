2508c5c2c963789e00eef592858ff5a3f370dac9
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-23 19:53:14 +0200

    Make Element support padding

--- src/components/shared/Element/Element.js
+++ src/components/shared/Element/Element.js
@@ -3,10 +3,11 @@ import styles from "./Element.module.scss";
 import classNames from "classnames";
 import upperFirst from "lodash/fp/upperFirst";
 
-const Element = ({ minWidth, className, children, ...props }) => {
+const Element = ({ minWidth, padding, className, children, ...props }) => {
   const classes = classNames([
     styles.element,
     styles[`element__minWidth${upperFirst(minWidth)}`],
+    styles[`element__padding${upperFirst(padding)}`],
     className,
   ]);
 
