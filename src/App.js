ce6f0a4f3f2460d90aef3046004f6e5e1a0dcb8c
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-23 18:52:50 +0200

    Move mobx shapes model store initialization upper in scope

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import "./App.css";
 import React from "react";
 import Page from "./components/shared/Page/Page";
 import Navigation from "./Navigation/Navigation";
-import ShapesModel from "./ShapeStore/ShapesModel/ShapesModel";
 import Shapes from "./ShapeStore/ShapesModel/Shapes";
-
-const shapesModel = new ShapesModel();
-
-shapesModel.getShapes();
 
 function App({ shapesModel }) {
   return (
