666f0542585ebbf94166804ba4ab284dbf0270a8
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-24 12:16:26 +0200

    Make PrimaryButton observer

--- src/components/shared/PrimaryButton/PrimaryButton.js
+++ src/components/shared/PrimaryButton/PrimaryButton.js
@@ -1,21 +1,14 @@
 import React from "react";
 import Button from "../../private/Button/Button";
 import BodyText from "../BodyText/BodyText";
+import { observer } from "mobx-react-lite";
 
-const PrimaryButton = ({ onClick, size, children, ...props }) => {
-  return (
-    <Button
-      onClick={onClick}
-      variant="primary"
-      size={size}
-      attention
-      {...props}
-    >
-      <BodyText font="body" size="md">
-        {children}
-      </BodyText>
-    </Button>
-  );
-};
+const PrimaryButton = ({ onClick, size, children, ...props }) => (
+  <Button onClick={onClick} variant="primary" size={size} attention {...props}>
+    <BodyText font="body" size="md">
+      {children}
+    </BodyText>
+  </Button>
+);
 
-export default PrimaryButton;
+export default observer(PrimaryButton);
