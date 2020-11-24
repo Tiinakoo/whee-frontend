bf72a18907cc29e201b951dd465f4cda8a43d944
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-24 12:17:28 +0200

    Add shopping cart icon available in Icon

--- src/components/shared/Icon/Icon.js
+++ src/components/shared/Icon/Icon.js
@@ -3,17 +3,33 @@ import styles from "./Icon.module.scss";
 import { ReactComponent as Triangle } from "../../../images/triangle.svg";
 import { ReactComponent as Rectangle } from "../../../images/rectangle.svg";
 import { ReactComponent as Circle } from "../../../images/circle.svg";
+import { ReactComponent as ShoppingCart } from "../../../images/cart-icon-2.svg";
+import classNames from "classnames";
+import upperFirst from "lodash/fp/upperFirst";
 
-const Icon = ({ iconName }) => {
-  switch (iconName) {
+const Icon = ({ iconName, navigation, className }) => {
+  const icon = upperFirst(iconName);
+
+  const classes = classNames([
+    styles.icon,
+    {
+      [styles.icon__navigation]: navigation,
+    },
+    className,
+  ]);
+
+  switch (icon) {
     case "Triangle":
-      return <Triangle className={styles.icon} />;
+      return <Triangle className={classes} />;
 
     case "Rectangle":
-      return <Rectangle className={styles.icon} />;
+      return <Rectangle className={classes} />;
 
     case "Circle":
-      return <Circle className={styles.icon} />;
+      return <Circle className={classes} />;
+
+    case "ShoppingCart":
+      return <ShoppingCart className={classes} />;
 
     default:
       return null;
